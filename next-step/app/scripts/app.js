11ef9b1b622ac89ef332b8dcce10715a1c7f534d
Mateusz Miodek <mmiodek@gmail.com>
2015-04-04 15:15:11 +0200

    First commit

---
+++ next-step/app/scripts/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nextStepApp
+ * @description
+ * # nextStepApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nextStepApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.sortable',
+    'LocalStorageModule'
+  ])
+  
+  .config(['localStorageServiceProvider', function (localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('ls');
+  }])
+
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ next-step/app/scripts/controllers/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('nextStepApp')
+  .controller('MainCtrl', function ($scope, localStorageService) {
+
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
+
+    $scope.addTodo = function () {
+      if ($scope.todo !== '') {
+        $scope.todos.push($scope.todo);
+        $scope.todo = '';
+      }
+    };
+
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
+
+  });
